783ca3a6efbe2b6a990c659c19c3e8bb6b550c45
ysmir <yongseokj2@gmail.com>
2025-02-03 22:24:49 +0900

    feat: 리디렉션 추가 - 루트 -> /posts 로 리디렉션

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import PostList from "@/app/ui/post/post-list";
-
 export default function Home() {
-  return (
-    <div className="max-w-3xl pt-20 md:pt-24 px-7 md:px-20 m-auto">
-      <PostList />
-    </div>
-  );
+  return;
 }
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,9 @@
+import PostList from "@/app/ui/post/post-list";
+
+export default function Page() {
+  return (
+    <div className="max-w-3xl pt-20 md:pt-24 px-7 md:px-20 m-auto">
+      <PostList />
+    </div>
+  );
+}
