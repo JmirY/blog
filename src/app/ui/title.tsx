04784fc1cd503df8b59206187eeed53a66e9c37b
ysmir <yongseokj2@gmail.com>
2025-02-03 17:11:42 +0900

    style: 타이틀 스타일 변경 - 자간 수정

--- src/app/ui/title.tsx
+++ src/app/ui/title.tsx
@@ -1,7 +1,7 @@
 export default function Title() {
   return (
     <div>
-      <p className="text-xl font-semibold tracking-wider">Dragonstone</p>
+      <p className="text-xl font-semibold tracking-wide">Dragonstone</p>
     </div>
   );
 }
