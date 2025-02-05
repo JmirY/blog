6c2cd23baf4dd7c91e642c45d68b4dd13dc91a92
ysmir <yongseokj2@gmail.com>
2025-02-05 10:39:54 +0900

    style: 작은 화면에서의 패딩 조정

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -4,7 +4,7 @@ export default function PostsLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="max-w-3xl pt-20 md:pt-24 px-7 md:px-20 m-auto">
+    <div className="max-w-3xl pt-[5.5rem] md:pt-24 px-7 md:px-20 m-auto">
       {children}
     </div>
   );
